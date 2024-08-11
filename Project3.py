aa848053e0f778cde11aa7485f059ec4171404f7
YashwantTanwar1 <yashwanttanwar1503@gmail.com>
2024-08-11 12:22:39 +0530

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -1,16 +1,16 @@
 import random
 import string
-
-def password(length):
-    while True:
-       if (length == 0):
-        return "thank you"
-        break 
+while True:
+    def password(length):
+    
+        
        characters = string.ascii_letters + string.digits + '@' + '#' + '&' + '*'
        print("Your pass is")
        return ''.join(random.choice(characters) for _ in range(length))
         
 
-length = int(input("Enter the length of the Password(0 for Exit): "))
-x = password(length)
-print(x)
+    length = int(input("Enter the length of the Password(0 for Exit): "))
+    if (length == 0):
+        break
+    x = password(length)
+    print(x)
