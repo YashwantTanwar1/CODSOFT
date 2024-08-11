ee4c7aa728c200f251cf0ae18a3f1323515674e6
YashwantTanwar1 <yashwanttanwar1503@gmail.com>
2024-08-11 11:46:49 +0530

    Create Project3.py

---
+++ Project3.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def password(length):
+    while True:
+       if (length == 0):
+        return "thank you"
+        break 
+       characters = string.ascii_letters + string.digits+'@'+'#'+'&'+'*'
+       print("Your pass is")
+       return ''.join(random.choice(characters) for _ in range(length))
+        
+
+length = int(input("Enter the length of the Password(0 for Exit): "))
+x = password(length)
+print(x)
